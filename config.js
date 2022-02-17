14ddc6f5598ec6b3db883274f24c5b97d61888f2
supuna <92195424+supuna28@users.noreply.github.com>
2022-02-17 06:03:31 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,45 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['94742134288']
+global.packname = '⚡♛𝛵𝛨𝛯 𝛣𝛩𝛵 𝛫𝛪𝛮𝐺𝐷𝛩𝛭♛⚡'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'only Admin Group!',
+    botAdmin: 'බොට් ඇඩ්මින් කෙනෙකු නොවේ',
+    owner: 'බොසාට විතරයි',
+    group: 'ගෲප් only!',
+    private: 'pc only!',
+    bot: 'Bot',
+    wait: 'Loading...',
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
